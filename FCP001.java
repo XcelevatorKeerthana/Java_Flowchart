ca88bf16fd66ac4723e6414cdaf3848c54576a5c
XcelevatorKeerthana <keerthana.2513@xcelevate.org>
2025-07-22 14:29:13 +0530

    FCP001

---
+++ FCP001.java
@@ -0,0 +1,9 @@
+public class FCP001{
+public static void main(String[]args){
+int birthDate=22;
+String birthMonth="May";
+int birthYear=2004;
+System.out.println(birthDate);
+System.out.println(birthMonth);
+System.out.println(birthYear);
+}}
