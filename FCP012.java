c17c02efbeeb11e7d94a98f12a92fade6dceb0be
XcelevatorKeerthana <keerthana.2513@xcelevate.org>
2025-07-22 22:22:21 +0530

    FCP012

---
+++ FCP012.java
@@ -0,0 +1,15 @@
+public class FCP012{
+public static void main(String[]args){
+int a=6;
+if(a==0)
+{
+System.out.println("Zero");
+}
+else if(a>0)
+{
+System.out.println("Positive");
+}
+else
+{
+System.out.println("Negative");
+}}}
