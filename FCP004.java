6c7ee127d56a7031b0574ea04fc7dc99943c979c
XcelevatorKeerthana <keerthana.2513@xcelevate.org>
2025-07-22 15:06:46 +0530

    FCP004

---
+++ FCP004.java
@@ -0,0 +1,15 @@
+public class FCP004{
+public static void main(String[]args){
+int a=12;
+int b=5;
+int c=a+b;
+int d=a-b;
+int e=a*b;
+float f=a/b;
+int g=a%b;;
+System.out.println(c);
+System.out.println(d);
+System.out.println(e);
+System.out.println(f);
+System.out.println(g);
+}}
