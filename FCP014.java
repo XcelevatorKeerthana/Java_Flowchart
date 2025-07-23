005f7faef41cc05b005e3c17c5f976265c22904e
XcelevatorKeerthana <keerthana.2513@xcelevate.org>
2025-07-23 23:02:00 +0530

    FCP014

---
+++ FCP014.java
@@ -0,0 +1,23 @@
+public class FCP014{
+public static void main(String[]args){
+int a=20;
+int b=3;
+int c=43;
+int max1=0;
+if(a>b)
+{
+max1=a;
+}
+else
+{
+max1=b;
+}
+if(max1>c)
+{
+System.out.println(max1);
+}
+else
+{
+System.out.println(c);
+}}
+}
