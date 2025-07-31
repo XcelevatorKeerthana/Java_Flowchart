f58116f900bbbff10c5d3b614184540ca12df4bc
XcelevatorKeerthana <keerthana.2513@xcelevate.org>
2025-08-01 01:25:52 +0530

    FCP013

--- FCP013.java
+++ FCP013.java
@@ -1,7 +1,9 @@
+import java.util.*;
 public class FCP013{
 public static void main(String[]args){
-int a=6;
+Scanner sc =new Scanner(System.in);
 int s=0;
+int a=sc.nextInt();
 if(a%2==0)
 {
 System.out.println(a);
